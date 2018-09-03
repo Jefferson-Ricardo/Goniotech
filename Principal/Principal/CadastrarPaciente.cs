6d5e26c57a36bbcd2f7691f7f1bf02f601b47f0f
jricardo <j.ricardossilva.jr@gmail.com>
2018-09-03 10:36:12 -0300

    Rascunho Tela Principal

---
+++ Principal/Principal/CadastrarPaciente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Principal
+{
+    public partial class CadastrarPaciente : Form
+    {
+        public CadastrarPaciente()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CadastrarPaciente_Load(object sender, EventArgs e)
+        {
+            cb_Sexo.Items.Add(new KeyValuePair<string, string>("Masculino", "0"));
+            cb_Sexo.Items.Add(new KeyValuePair<string, string>("Feminino", "1"));
+            cb_Sexo.DisplayMember = "key";
+            cb_Sexo.ValueMember = "value";
+
+            cb_area.Items.Add(new KeyValuePair<string, string>("Neurologia", "0"));
+            cb_area.Items.Add(new KeyValuePair<string, string>("Traumatologia", "1"));
+            cb_area.Items.Add(new KeyValuePair<string, string>("Reumatologia", "2"));
+            cb_area.DisplayMember = "key";
+            cb_area.ValueMember = "value";
+
+
+
+        }
+    }
+}
---
+++ Principal/Principal/LocalizarPaciente.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Principal
+{
+    public partial class LocalizarPaciente : Form
+    {
+
+        public LocalizarPaciente()
+        {
+            InitializeComponent();
+        }
+
+        private void LocalizarPaciente_Load(object sender, EventArgs e)
+        {
+            ListViewItem lv1 = new ListViewItem("Barrigudinha Seleida", 0);
+            lv1.SubItems.Add("23");
+            lv1.SubItems.Add("Neurologia");
+            lv1.SubItems.Add("Luciana Soares");
+
+            ListViewItem lv2 = new ListViewItem("Manoel Sovaco de Gambar", 1);
+            lv2.SubItems.Add("24");
+            lv2.SubItems.Add("Traumatologia");
+            lv2.SubItems.Add("Danielle Ferreira");
+
+            ListViewItem lv3 = new ListViewItem("Antônio Morrendo Das Dores", 2);
+            lv3.SubItems.Add("38");
+            lv3.SubItems.Add("Ortopedia");
+            lv3.SubItems.Add("Francisco das Chagas");
+
+            lv_busca.Items.Add(lv1);
+            lv_busca.Items.Add(lv2);
+            lv_busca.Items.Add(lv3);
+
+
+        }
+
+        private void txt_avaliar_Click(object sender, EventArgs e)
+        {
+            string selectName = lv_busca.SelectedItems[0].SubItems[0].Text;
+            string area = lv_busca.SelectedItems[0].SubItems[2].Text;
+            string fisio = lv_busca.SelectedItems[0].SubItems[3].Text;
+
+            using (var mainWindow = new MainWindow())
+            {
+                mainWindow.ShowDialog();
+            }
+
+
+        }
+
+
+
+    }
+}
